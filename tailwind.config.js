a161cef39c3bb594561791a198760ed0b1f4c952
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-04-17 19:43:28 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: ['./src/*.{html,js}',
+  './index.html'],
+  theme: {
+    screens: {
+      'xs': '480px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'tangerine': '#ff9500',
+        'magenta': '#8b1c3f',
+        'storm': '#00008c',
+      },
+    },
+  },
+  plugins: [],
+}
+
