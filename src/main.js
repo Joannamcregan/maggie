9385aa5c7aabd8ae3ec754e2626eadbd04d7e977
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-04-18 15:52:37 -0400

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -3,21 +3,25 @@ const lightsUp = document.getElementById('lightsUp');
 const lightsOut = document.getElementById('lightsOut');
 const readExcerptStation = document.getElementById('readExcerptStation');
 const readExcerptFoleys = document.getElementById('readExcerptFoleys');
-
-// var isDark = false;
+const stationExcerpt = document.getElementById('stationExcerpt');
+const foleyExcerpt = document.getElementById('foleyExcerpt');
 
 lightsUp.addEventListener('click', toggleLight.bind(this));
 lightsOut.addEventListener('click', toggleLight.bind(this));
+readExcerptStation.addEventListener('click', toggleExcerptVisibility.bind(this, stationExcerpt, readExcerptStation));
+readExcerptFoleys.addEventListener('click', toggleExcerptVisibility.bind(this, foleyExcerpt, readExcerptFoleys));
 
 function toggleLight(){
-    // if (isDark){
-    //     body.classList.remove('dark');
-    //     isDark = false;
-    // } else {
-    //     body.classList.add('dark');
-    //     isDark = true;
-    // }
     body.classList.toggle('dark');
     lightsUp.classList.toggle('hidden');
     lightsOut.classList.toggle('hidden');
+}
+
+function toggleExcerptVisibility(excerpt, button){
+    excerpt.classList.toggle('hidden');
+    if (button.innerText == 'read an excerpt'){
+        button.innerText = 'hide the excerpt'
+    } else {
+        button.innerText = 'read an excerpt'
+    }
 }
