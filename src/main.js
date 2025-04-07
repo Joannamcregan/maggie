3d13d147cf9da063f9e6cfdc506cc3aa2f1e05f6
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-04-07 16:02:13 -0400

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -3,13 +3,19 @@ const lightsUp = document.getElementById('lightsUp');
 const lightsOut = document.getElementById('lightsOut');
 const readExcerptStation = document.getElementById('readExcerptStation');
 const readExcerptFoleys = document.getElementById('readExcerptFoleys');
+const readPlayLeonora = document.getElementById('readPlayLeonora');
+const readPlayLineCooks = document.getElementById('readPlayLineCooks');
 const stationExcerpt = document.getElementById('stationExcerpt');
 const foleyExcerpt = document.getElementById('foleyExcerpt');
+const leonoraPlay = document.getElementById('leonoraPlay');
+const lineCookPlay = document.getElementById('lineCookPlay');
 
 lightsUp.addEventListener('click', toggleLight.bind(this));
 lightsOut.addEventListener('click', toggleLight.bind(this));
 readExcerptStation.addEventListener('click', toggleExcerptVisibility.bind(this, stationExcerpt, readExcerptStation));
 readExcerptFoleys.addEventListener('click', toggleExcerptVisibility.bind(this, foleyExcerpt, readExcerptFoleys));
+readPlayLeonora.addEventListener('click', toggleExcerptVisibility.bind(this, leonoraPlay));
+readPlayLineCooks.addEventListener('click', toggleExcerptVisibility.bind(this, lineCookPlay));
 
 function toggleLight(){
     body.classList.toggle('dark');
